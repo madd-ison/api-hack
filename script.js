b54b7481195c76ab5fe5f8b4e2bb537404dd7ca3
Maddison Murphy <maddison@puppyteeth.org>
2020-06-08 18:01:36 -0400

    added some params

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const searchUrl = 'https://api.edamam.com/search'
+const searchEsp = 'https://test-es.edamam.com/search'
+const appKey = 
+'fd967e623643b511adeedd8cb6db33ec'	
+
+const params {
+    q: searchTerm,
+    r: returnInfo,
+    app_id: 8b12ba8f,
+    app_key: appKey,
+}
