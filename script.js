d11b61825efe183959c37b1cab89c762e644703e
Maddison Murphy <maddison@puppyteeth.org>
2020-06-09 14:13:54 -0400

    jquery outline

--- script.js
+++ script.js
@@ -4,10 +4,32 @@ const searchUrl = 'https://api.edamam.com/search'
 const searchEsp = 'https://test-es.edamam.com/search'
 const appKey = 
 'fd967e623643b511adeedd8cb6db33ec'	
+const appId = '8b12ba8f'
 
-const params {
+const params = {
     q: searchTerm,
     r: returnInfo,
-    app_id: 8b12ba8f,
+    app_id: appId,
     app_key: appKey,
 }
+
+function formatParams(){
+    
+}
+
+function getRecipes(){
+
+}
+
+function getEspanol(){
+
+}
+
+function displayRecipes(){
+
+}
+
+function processInput(){
+
+}
+
