2d79e5def3d589e142f8425e72dbdf9bd9031383
madd-ison <65075658+madd-ison@users.noreply.github.com>
2020-06-14 11:47:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -64,6 +64,6 @@ function processEnInput(){
     const q = $('#en-search-term').val()
     findRecipes(q)
     $('#back').removeClass('hidden')
-    $('#contact-btn).addClass('hidden')
+    $('#contact-btn').addClass('hidden')
   })
 }
